8843e585187b825460f418c1389ec4e2d3ec6dd6
Gustavo Schneider <gusta.schneider123@gmail.com>
2024-11-09 17:27:31 -0300

    Initial Front-end code

---
+++ index.js
@@ -0,0 +1,14 @@
+document.querySelectorAll('.btn[data-toggle="collapse"]').forEach(button => {
+    button.addEventListener('click', function() {
+        const icon = this.querySelector('.toggle-icon');
+        
+        // Alterna entre fa-caret-right e fa-caret-down
+        if (icon.classList.contains('fa-caret-right')) {
+            icon.classList.remove('fa-caret-right');
+            icon.classList.add('fa-caret-down');
+        } else {
+            icon.classList.remove('fa-caret-down');
+            icon.classList.add('fa-caret-right');
+        }
+    });
+});
