0167660dfc009a20f2beb0cbc3f37ff9507243a4
gugsguga844 <gusta.schneider123@gmail.com>
2024-11-26 17:02:34 -0300

    First Commit

--- public/assets/js/application.js
+++ public/assets/js/application.js
@@ -1,26 +1,14 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const imagePreviewInput = document.getElementById("image_preview_input");
-  const preview = document.getElementById("image_preview");
-  const imagePreviewSubmit = document.getElementById("image_preview_submit");
-
-  if (!(imagePreviewInput && preview)) return;
-
-  imagePreviewInput.style.display = "none";
-  imagePreviewSubmit.style.display = "none";
-
-  preview.addEventListener("click", function () {
-    imagePreviewInput.click();
-  });
-
-  imagePreviewInput.addEventListener("change", function (event) {
-    const file = event.target.files[0];
-    if (file) {
-      const reader = new FileReader();
-      reader.onload = function (e) {
-        document.getElementById("image_preview").src = e.target.result;
-        imagePreviewSubmit.style.display = "block";
-      };
-      reader.readAsDataURL(file);
-    }
+document.querySelectorAll('.btn[data-toggle="collapse"]').forEach(button => {
+  button.addEventListener('click', function() {
+      const icon = this.querySelector('.toggle-icon');
+      
+      // Alterna entre fa-caret-right e fa-caret-down
+      if (icon.classList.contains('fa-caret-right')) {
+          icon.classList.remove('fa-caret-right');
+          icon.classList.add('fa-caret-down');
+      } else {
+          icon.classList.remove('fa-caret-down');
+          icon.classList.add('fa-caret-right');
+      }
   });
 });
