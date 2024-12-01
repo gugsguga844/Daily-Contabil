717afbd73e615cc9756cfe9e31a944c5e0b4013e
gugsguga844 <gusta.schneider123@gmail.com>
2024-12-01 18:07:14 -0300

    basic login form

--- config/routes.php
+++ config/routes.php
@@ -2,6 +2,15 @@
 
 use App\Controllers\HomeController;
 use Core\Router\Route;
+use App\Controllers\AuthenticationsController;
 
 // Authentication
 Route::get('/', [HomeController::class, 'index'])->name('root');
+
+// Login
+Route::get('/login', [AuthenticationsController::class, 'new'])->name('users.login');
+Route::post('/login', [AuthenticationsController::class, 'authenticate'])->name('users.authenticate');
+// Logout
+Route::get('/logout', [AuthenticationsController::class, 'destroy'])->name('users.logout');
+
+
---
+++ database/schema.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(50) UNIQUE NOT NULL,
+    encrypted_password VARCHAR(255) NOT NULL,
+    avatar_name VARCHAR(65)
+);
+
+DROP TABLE IF EXISTS problems;
